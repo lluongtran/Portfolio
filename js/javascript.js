3599d86ff8f392df1d001dd0d1bf2f27e52ee640
lluongtran <35949453+lluongtran@users.noreply.github.com>
2019-03-31 22:08:13 -0700

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,18 @@ function onClick(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
 }
+
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
